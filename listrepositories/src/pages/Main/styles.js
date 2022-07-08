14129b58091286eaba539938e609923be9b1425c
H. Sampaio <higor_sampaio@hotmail.com>
2022-07-07 21:40:19 -0300

    Initial commit

---
+++ listrepositories/src/pages/Main/styles.js
@@ -0,0 +1,68 @@
+import styled, { keyframes } from 'styled-components';
+
+export const Container = styled.div`
+  background: #fff;
+  max-width: 700px;
+  padding: 30px;
+  margin: 80px auto;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+
+  h1 {
+    font-size: 20px;
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+
+    svg {
+      margin-right: 10px;
+    }
+  }
+`;
+export const Form = styled.form`
+  display: flex;
+  margin-top: 30px;
+  flex-direction: row;
+
+  input {
+    flex: 1;
+    border: 1px solid #eee;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+`;
+
+const rotate = keyframes`
+  from{
+    transform: rotate(0deg)
+  }
+
+  to{
+    transform: rotate(360deg)
+  }
+`;
+
+export const SubmitButton = styled.button.attrs(props => ({
+  type: 'submit',
+  disabled: props.loading,
+}))`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background: #7159c1;
+  border: 0;
+  padding: 0 15px;
+  margin-left: 10px;
+  border-radius: 4px;
+
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+
+    svg {
+      animation: ${rotate} 2s linear infinite;
+    }
+  }
+`;
